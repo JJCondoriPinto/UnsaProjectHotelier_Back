7102177a706cb2cc8c12639bb125becfced075e7
JuanJoseMCP <juanjosecondoripinto@gmail.com>
2023-08-06 13:32:59 -0500

    w

--- python/entrypoint.sh
+++ python/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Migrar la base de datos
 # python manage.py migrate
 
-# Crear un superusuario con información automática (cambiar USERNAME y PASSWORD según tus necesidades)
 # echo "from Apis.models import User; User.objects.create_superuser('fastbook@fastbooking.com', 'gWqzP0op6FYSWP2cUf0193mkM', nombres='Gerente', apellidos='Gerente', dni='222222', telefono='938201233')" | python manage.py shell
 
 # Iniciar el servidor
