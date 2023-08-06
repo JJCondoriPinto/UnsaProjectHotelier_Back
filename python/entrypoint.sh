7632db89d13c7121f403f450de4e675974678ecf
JuanJoseMCP <juanjosecondoripinto@gmail.com>
2023-08-06 15:02:13 -0500

    rev

--- python/entrypoint.sh
+++ python/entrypoint.sh
@@ -5,5 +5,6 @@
 # echo "from Apis.models import User; User.objects.create_superuser('fastbook@fastbooking.com', 'gWqzP0op6FYSWP2cUf0193mkM', nombres='Gerente', apellidos='Gerente', dni='222222', telefono='938201233')" | python manage.py shell
 
 # Iniciar el servidor
-gunicorn FastBooking.wsgi:application --bind 0.0.0.0:8000
 service nginx start
+
+gunicorn FastBooking.wsgi:application --bind 0.0.0.0:8000
