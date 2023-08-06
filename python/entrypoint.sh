350c4767a22a8e9b6980690af762797bc811a13f
JuanJoseMCP <juanjosecondoripinto@gmail.com>
2023-08-06 13:31:55 -0500

    migrations not

--- python/entrypoint.sh
+++ python/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Migrar la base de datos
-python manage.py migrate
+# python manage.py migrate
 
 # Crear un superusuario con información automática (cambiar USERNAME y PASSWORD según tus necesidades)
-echo "from Apis.models import User; User.objects.create_superuser('fastbook@fastbooking.com', 'gWqzP0op6FYSWP2cUf0193mkM', nombres='Gerente', apellidos='Gerente', dni='222222', telefono='938201233')" | python manage.py shell
+# echo "from Apis.models import User; User.objects.create_superuser('fastbook@fastbooking.com', 'gWqzP0op6FYSWP2cUf0193mkM', nombres='Gerente', apellidos='Gerente', dni='222222', telefono='938201233')" | python manage.py shell
 
 # Iniciar el servidor
 python gunicorn FastBooking.wsgi:application --bind 0.0.0.0:8000
