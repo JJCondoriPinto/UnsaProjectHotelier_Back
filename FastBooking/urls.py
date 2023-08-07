e644b52bf20f1e7eef1ee850e3e351a3d6be8fb0
JuanJoseMCP <juanjosecondoripinto@gmail.com>
2023-08-06 20:46:00 -0500

    redirect

--- FastBooking/urls.py
+++ FastBooking/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
+from django.views.generic import RedirectView
 from Apis.views import CustomAuthToken
 from rest_framework import urls
 
 urlpatterns = [
-    path('', redirect(to='admin/', permanent=True)),
+    path('', RedirectView.as_view(url='admin/', permanent=True)),
     path('admin/', admin.site.urls),
     path('api/v2/', include('Apis.urls')),
     path('api/auth', include(urls)),
